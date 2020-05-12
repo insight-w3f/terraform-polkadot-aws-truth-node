b6eff88cb373e967583fdea295312e73cbd1f06e
Richard Mah <dick.mah@gmail.com>
2020-05-12 16:36:09 -0400

    update default test

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,4 +1,16 @@
+variable "public_key_path" {}
+variable "private_key_path" {}
 
-module "defaults" {
-  source = "../.."
+module "network" {
+  source = "github.com/insight-infrastructure/terraform-aws-polkadot-network.git?ref=master"
+  sentry_enabled = true
+  num_azs = 1
+}
+
+module "default" {
+  source            = "../.."
+  public_key        = file(var.public_key_path)
+  subnet_id         = module.network.public_subnets[0]
+  security_group_id = module.network.sentry_security_group_id
+  private_key_path  = var.private_key_path
 }
